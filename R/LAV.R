e5c95244a8229153f581d5c91671324a58f62620
oar <spyqqqdia@yahoo.com>
2020-10-17 05:02:37 +0200

    Added LAV knife with large modified tip for optimal half spin grip.

--- R/LAV.R
+++ R/LAV.R
@@ -311,7 +311,7 @@ knife_LAV_lt_27cm <- function(f,th,d){
   msg <- "\n\nLayer tip:\n"
   msg <- paste(msg,"\nLine 2 points:\n")
   msg <- paste(msg,f*190,",",f*40,"\n")
-  msg <- paste(msg,f*280,",",f*30,"\n")
+  msg <- paste(msg,f*280,",",f*33,"\n")
   msg <- paste(msg,f*200,",",f*10,"\n")
   
   msg <- paste(msg,"\nLayer tip_neck:\n")
@@ -361,4 +361,79 @@ knife_LAV_lt_27cm <- function(f,th,d){
   
   cat("\nFinished, result in",outFile,"\n")
 }
+
+
+
+
+# Writes out CAD points for LibreCAD and computes weight.
+# Version with medium tip (mt).
+#
+# @param f scale factor. Applied to normal scale = 25cm long.
+# @param d steel density kg/dm^3 = 1e6 kg/mm^3
+# @param th thickness in mm
+#
+# Standard length/thickness: 28cm, 6mm, weight: 260g
+# Scale factor is applied only to length and width, not thickness.
+#
+knife_ltm_LAV_28cm <- function(f,th,d){
+  
+  msg <- "\n\nLayer tip:\n"
+  msg <- paste(msg,"\nLine 2 points:\n")
+  msg <- paste(msg,f*225,",",f*40,"\n")
+  msg <- paste(msg,f*290,",",f*33,"\n")
+  msg <- paste(msg,f*210,",",f*10,"\n")
+  
+  msg <- paste(msg,"\nLayer tip_neck:\n")
+  msg <- paste(msg,"\nSpline through points:\n")
+  msg <- paste(msg,f*225,",",f*40,"\n")
+  msg <- paste(msg,f*195,",",f*36,"\n")
+  msg <- paste(msg,f*175,",",f*35,"\n")
+  msg <- paste(msg,"\nSpline through points:\n")
+  msg <- paste(msg,f*210,",",f*10,"\n")
+  msg <- paste(msg,f*180,",",f*18,"\n")
+  msg <- paste(msg,f*160,",",f*20,"\n")
+  
+  msg <- paste(msg,"\nLayer middle:\n")
+  msg <- paste(msg,"\nLine 2 points:\n")
+  msg <- paste(msg,f*175,",",f*35,"\n")
+  msg <- paste(msg,f*70,",",f*35,"\n")
+  msg <- paste(msg,"\nLine 2 points:\n")
+  msg <- paste(msg,f*160,",",f*20,"\n")
+  msg <- paste(msg,f*100,",",f*20,"\n")
+  
+  
+  msg <- paste(msg,"\nLayer handle:\n")
+  msg <- paste(msg,"\nSpline through points:\n")
+  msg <- paste(msg,f*70,",",f*35,"\n")
+  msg <- paste(msg,f*42,",",f*36.5,"\n")
+  msg <- paste(msg,f*20,",",f*36,"\n")
+  msg <- paste(msg,f*10,",",f*30,"\n")
+  msg <- paste(msg,f*10,",",f*18,"\n")
+  msg <- paste(msg,f*20,",",f*10,"\n")
+  msg <- paste(msg,f*50,",",f*8,"\n")
+  msg <- paste(msg,f*80,",",f*17,"\n")
+  msg <- paste(msg,f*100,",",f*20,"\n")
+  
+  msg <- paste(msg,"\n\nCircle:\n")
+  msg <- paste(msg,f*24,",",f*23,"\n")
+  msg <- paste(msg,"Radius = 3mm\n")
+  
+  ID <- "LAV_ltm_28cm"
+  outFile <- paste("knives/",ID,"_th=",th,"_f=",f,".txt",sep="")
+  cat(msg,file=outFile,append=FALSE)
+  
+  # approximate weight (normalized size)
+  weight <- f*f*0.260*(th/6)*(d/7.9)
+  msg <- paste("\n\nLength: ",f*27,"cm, thickness: ",th,"mm\n",sep="")
+  msg <- paste(msg,"Weight: ",round(weight,3)," kg.\n",sep="")
+  cat(msg,file=outFile,append=TRUE)
+  
+  cat("\nFinished, result in",outFile,"\n")
+}
+
+
+
+
+
+
 
